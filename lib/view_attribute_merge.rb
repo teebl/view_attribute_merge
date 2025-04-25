39af6c716fb5a12ec5632162ffa1cca023c549c5
Trevor Seibel <trevor.seibel@gmail.com>
2025-04-25 10:03:18 -0700

    removed actionview

--- lib/view_attribute_merge.rb
+++ lib/view_attribute_merge.rb
@@ -3,7 +3,6 @@
 require_relative "view_attribute_merge/version"
 
 module ViewAttributeMerge
-  include ActionView::Helpers::TagHelper
   class Error < StandardError; end
 
   def self.mega_merge(*hashes)

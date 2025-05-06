ecadc0a98143807e234ccc944a9c60e4318491ad
Trevor Seibel <trevor.seibel@gmail.com>
2025-05-06 16:55:45 -0700

    restructuring

--- lib/view_attribute_merge.rb
+++ lib/view_attribute_merge.rb
@@ -5,15 +5,24 @@ require_relative "view_attribute_merge/version"
 module ViewAttributeMerge
   class Error < StandardError; end
 
-  def self.mega_merge(*hashes)
-    hashes
-      .flatten
-      .reverse
-      .reduce({}, :merge)
-      .then { |merged| normalize_options(merged) }
+  def self.attr_merge(*options)
+    output = {}
+
+    options.reverse_each { |option_blob| process_blob(option_blob, output) }
+
+    output
   end
 
-  def self.normalize_options(hash)
-    hash
+  def self.process_blob(option_blob, output)
+    case option_blob
+    when Hash
+      process_hash(option_blob, output)
+    when Array
+      option_blob.reverse_each { |blob| process_blob(blob, output) }
+    end
+  end
+
+  def self.process_hash(hash, output)
+    hash.each_pair { |key, value| output[key] = value }
   end
 end
--- spec/view_attribute_merge_spec.rb
+++ spec/view_attribute_merge_spec.rb
@@ -10,28 +10,28 @@ RSpec.describe ViewAttributeMerge do
       sample = [{ foo: "bar" }, { bar: "baz" }]
       result = { foo: "bar", bar: "baz" }
 
-      expect(ViewAttributeMerge.mega_merge(*sample)).to eq(result)
+      expect(ViewAttributeMerge.attr_merge(*sample)).to eq(result)
     end
 
     it "merges many hashes, all passed in as arguments" do
       sample = [{ foo: "bar" }, { bar: "baz" }, { boosh: "bim" }, bam: "bop"]
       result = { foo: "bar", bar: "baz", boosh: "bim", bam: "bop" }
 
-      expect(ViewAttributeMerge.mega_merge(*sample)).to eq(result)
+      expect(ViewAttributeMerge.attr_merge(*sample)).to eq(result)
     end
 
     it "if an array of hashes is passed in, it is flattened and incorporated" do
       sample = [[{ foo: "bar" }, { bar: "baz" }], [{ boosh: "bim" }, bam: "bop"]]
       result = { foo: "bar", bar: "baz", boosh: "bim", bam: "bop" }
 
-      expect(ViewAttributeMerge.mega_merge(*sample)).to eq(result)
+      expect(ViewAttributeMerge.attr_merge(*sample)).to eq(result)
     end
 
     it "prioritizes values in descending order" do
       sample = [[{ foo: "right" }, { foo: "wrong" }], [{ bar: "accurate" }, bar: "incorrect"]]
       result = { foo: "right", bar: "accurate" }
 
-      expect(ViewAttributeMerge.mega_merge(*sample)).to eq(result)
+      expect(ViewAttributeMerge.attr_merge(*sample)).to eq(result)
     end
   end
 
@@ -40,7 +40,7 @@ RSpec.describe ViewAttributeMerge do
       sample = [[{ data: { "foo": "bar" } }, { data: { "baz": "bop" } }]]
       result = { foo: "right", bar: "accurate" }
 
-      expect(ViewAttributeMerge.mega_merge(*sample)).to eq(result)
+      expect(ViewAttributeMerge.attr_merge(*sample)).to eq(result)
     end
   end
 
